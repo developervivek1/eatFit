55873a3beb99654871a704a1b1e461d2e10cd024
developervivek1 <vivekpathak.emmy1234@gmail.com>
2022-08-10 10:58:25 +0530

    eatFit init

---
+++ js/script.js
@@ -0,0 +1,47 @@
+window.onload = function () {
+    var myPane = new CupertinoPane(
+      '.cupertino-pane', // Pane container selector
+      { 
+        parentElement: 'body', // Parent container
+        upperThanTop: true,
+        buttonDestroy: false,
+        backdrop: true,
+        breaks: {
+            top:{ enabled: true, height: 770, bounce: true },
+            middle: { enabled: true, height: 450, bounce: true },
+            bottom: { enabled: true, height: 180 },
+        },
+        // onDrag: () => console.log('Drag event'),
+        onBackdropTap: () => {
+          console.log('onBackdropTap');
+          myPane.moveToBreak('bottom');
+          myPane.backdrop({show: false});
+        },
+        onTransitionEnd: () => {
+          if (myPane.currentBreak() === 'bottom') {
+            myPane.backdrop({show: false});
+          } else {
+            myPane.backdrop({show: true});
+          }
+        },
+        // onDragEnd: () => {
+        //   if (myPane.currentBreak() === 'bottom') {
+        //     myPane.backdrop({show: false});
+        //   } else {
+        //     myPane.backdrop({show: true});
+        //   }
+        // },
+      }
+    );
+    myPane.present({animate: true}).then(res => {
+      console.log('present')
+    });
+};
+
+
+
+
+
+
+// today date
+
