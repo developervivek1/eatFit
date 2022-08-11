5e98ed738ad45b2190db8628b6732e75861e8968
developervivek1 <vivekpathak.emmy1234@gmail.com>
2022-08-11 22:18:37 +0530

    script

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ window.onload = function () {
         breaks: {
             top:{ enabled: true, height: 670, bounce: true },
             middle: { enabled: true, height: 450, bounce: true },
-            bottom: { enabled: true, height: 180 },
+            bottom: { enabled: false, height: 180 },
         },
         // onDrag: () => console.log('Drag event'),
         onBackdropTap: () => {
@@ -43,5 +43,20 @@ window.onload = function () {
 
 
 
-// today date
+// tabs
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
